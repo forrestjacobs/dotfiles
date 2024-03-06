6d40023ab5c0e595a49d23bdf5b63ec1258c8983
Forrest Jacobs <forrestjacobs@gmail.com>
2024-03-05 22:29:33 -0500

    Add support for apt

--- install.sh
+++ install.sh
@@ -2,6 +2,9 @@
 
 if command -v brew &> /dev/null; then
   brew bundle --no-lock
+elif command -v apt-get &> /dev/null; then
+  sudo apt-get update
+  xargs -a packages.txt sudo apt-get install -y
 fi
 
 mkdir -p "${HOME}/.config/git";
