4c33c0c76e57b9363f837abd395164c282f033e9
Forrest Jacobs <forrestjacobs@gmail.com>
2024-03-07 17:54:58 -0500

    Add notional flake

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  outputs = { ... }: {
+    homeModules.default = ({ lib, pkgs, config, ... }: {
+      home.stateVersion = "23.11";
+
+      home.packages = [
+        pkgs.bat
+        pkgs.eza
+        pkgs.fish
+        pkgs.fzf
+        pkgs.helix
+        pkgs.patchutils
+        pkgs.zoxide
+      ];
+
+      programs.git = {
+        enable = true;
+        userName = "Forrest Jacobs";
+        userEmail = lib.mkDefault "forrestjacobs@gmail.com";
+        extraConfig = {
+          pull.ff = "only";
+          init.defaultBranch = "main";
+        };
+      };
+
+      home.file."${config.xdg.configHome}" = {
+        source = ./config;
+        recursive = true;
+      };
+    });
+  };
+}
