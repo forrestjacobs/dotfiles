a2c77eedb7a7b7a55844bde6cd997349335bed62
Forrest Jacobs <forrestjacobs@gmail.com>
2025-02-01 23:33:36 -0500

    `brew shellenv` is fast enough now

--- config/fish/conf.d/50-main.fish
+++ config/fish/conf.d/50-main.fish
@@ -5,13 +5,7 @@ set -xg XDG_CACHE_HOME "$HOME"/.cache
 
 # homebrew
 if test -x /opt/homebrew/bin/brew
-  # from `brew shellenv`
-  set -gx HOMEBREW_PREFIX "/opt/homebrew";
-  set -gx HOMEBREW_CELLAR "/opt/homebrew/Cellar";
-  set -gx HOMEBREW_REPOSITORY "/opt/homebrew";
-  set -q PATH; or set PATH ''; set -gx PATH "/opt/homebrew/bin" "/opt/homebrew/sbin" $PATH;
-  set -q MANPATH; or set MANPATH ''; set -gx MANPATH "/opt/homebrew/share/man" $MANPATH;
-  set -q INFOPATH; or set INFOPATH ''; set -gx INFOPATH "/opt/homebrew/share/info" $INFOPATH;
+  /opt/homebrew/bin/brew shellenv
 end
 
 # bat
