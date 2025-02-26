cb4f01100b227a7efa263f37fce8fe1b117192a4
Forrest Jacobs <forrestjacobs@gmail.com>
2025-02-25 23:12:09 -0500

    Only install packages with brew

--- install.sh
+++ install.sh
@@ -4,19 +4,19 @@ mkdir -p ~/.local/bin
 
 if command -v brew &> /dev/null; then
   brew bundle --no-lock
-elif command -v apt-get &> /dev/null; then
-  sudo apt-get update
-  sudo apt-get install -y fish stow
-fi
 
-stow --no-folding -t "${HOME}/.config" config
-stow --no-folding -t "${HOME}/.local/bin" bin
+  stow --no-folding -t "${HOME}/.config" config
+  stow --no-folding -t "${HOME}/.local/bin" bin
 
-if [[ $(basename "$SHELL") != "fish" ]]; then
-  echo "Change your shell to fish by running:"
-  fish_path=$(which fish)
-  if ! grep -q -F "${fish_path}" /etc/shells; then
-    echo "  echo \"${fish_path}\" | sudo tee -a /etc/shells"
+  if [[ $(basename "$SHELL") != "fish" ]]; then
+    echo "Change your shell to fish by running:"
+    fish_path=$(which fish)
+    if ! grep -q -F "${fish_path}" /etc/shells; then
+      echo "  echo \"${fish_path}\" | sudo tee -a /etc/shells"
+    fi
+    echo "  chsh -s \"${fish_path}\""
   fi
-  echo "  chsh -s \"${fish_path}\""
+else
+  rsync -a config "${HOME}/.config"
+  rsync -a bin "${HOME}/.local/bin"
 fi
