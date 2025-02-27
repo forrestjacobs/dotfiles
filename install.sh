9c2730460bb35ca5216fc9ddb38e4f344770491f
Forrest Jacobs <forrestjacobs@gmail.com>
2025-02-27 00:00:31 +0000

    Clean up install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
+set -e
 
 mkdir -p ~/.local/bin
 
 if command -v brew &> /dev/null; then
   brew bundle --no-lock
 
-  stow --no-folding -t "${HOME}/.config" config
-  stow --no-folding -t "${HOME}/.local/bin" bin
-
+  # We're only setting up fish from homebrew because other package managers's
+  # fishes could be too old.
   if [[ $(basename "$SHELL") != "fish" ]]; then
     echo "Change your shell to fish by running:"
     fish_path=$(which fish)
@@ -16,7 +16,18 @@ if command -v brew &> /dev/null; then
     fi
     echo "  chsh -s \"${fish_path}\""
   fi
-else
-  rsync -a "config/" "${HOME}/.config"
-  rsync -a "bin/" "${HOME}/.local/bin"
 fi
+
+inst() {
+  if command -v stow &> /dev/null; then
+    stow --ignore "\.DS_Store" --no-folding -t "${2}" "{$1}"
+  elif command -v rsync &> /dev/null; then
+    rsync -a "${1}/" "${2}"
+  else
+    echo "Could not install dotfiles. Install stow or rsync, then try again."
+    exit 1
+  fi
+}
+
+inst config "${HOME}/.config"
+inst bin "${HOME}/.local/bin"
