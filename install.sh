bdad1c97ae1f654484e182f10507be817d58469b
Forrest Jacobs <forrestjacobs@gmail.com>
2025-06-11 22:23:03 -0400

    Force homebrew and fish

--- install.sh
+++ install.sh
@@ -3,36 +3,35 @@ set -e
 
 mkdir -p ~/.local/bin
 
-if command -v brew &> /dev/null; then
-  brew bundle
+if !command -v brew &> /dev/null; then
+  echo "Installing homebrew"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
 
-  # We're only setting up fish from homebrew because other package managers's
-  # fishes could be too old.
-  if [[ $(basename "$SHELL") != "fish" ]]; then
-    echo "Change your shell to fish by running:"
-    fish_path=$(which fish)
-    if ! grep -q -F "${fish_path}" /etc/shells; then
-      echo "  echo \"${fish_path}\" | sudo tee -a /etc/shells"
-    fi
-    echo "  chsh -s \"${fish_path}\""
+OS="$(uname)"
+if [[ "${OS}" == "Darwin" ]]; then
+  PATH=/opt/homebrew/bin:$PATH
+else
+  PATH=/home/linuxbrew/.linuxbrew/bin:$PATH
+fi
+
+brew bundle
+
+if [[ $(basename "$SHELL") != "fish" ]]; then
+  echo "Change your shell to fish by running:"
+  fish_path=$(type -P fish)
+  if ! grep -q -F "${fish_path}" /etc/shells; then
+    echo "Adding ${fish_path} to /etc/shells"
+    echo "${fish_path}" | sudo tee -a /etc/shells
   fi
+  echo "Changing shell to ${fish_path}"
+  chsh -s "${fish_path}"
 fi
 
 inst() {
-  if command -v stow &> /dev/null; then
-    find -L "${2}" -type l -delete
-    stow --ignore "\.DS_Store" --no-folding -t "${2}" "${1}"
-  elif command -v rsync &> /dev/null; then
-    rsync -a "${1}/" "${2}"
-  else
-    echo "Could not install dotfiles. Install stow or rsync, then try again."
-    exit 1
-  fi
+  find -L "${2}" -type l -delete
+  stow --ignore "\.DS_Store" --no-folding -t "${2}" "${1}"
 }
 
 inst config "${HOME}/.config"
 inst bin "${HOME}/.local/bin"
-
-if ! grep -qF "forrest/rc.bash" ~/.bashrc &> /dev/null; then
-  echo "[ -f ~/.config/forrest/rc.bash ] && . ~/.config/forrest/rc.bash" >> ~/.bashrc
-fi
