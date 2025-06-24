db1e22ffde1cb708a457a41c4c1dab1648d6f023
Forrest Jacobs <forrestjacobs@gmail.com>
2025-06-24 23:18:03 +0000

    Chill for Debian containers

--- install.sh
+++ install.sh
@@ -1,24 +1,25 @@
 #!/usr/bin/env bash
 set -e
 
-if [[ $(uname) == "Darwin" ]]; then
-  brew_path=/opt/homebrew/bin
-elif [[ $(lsb_release -sd) == "Debian GNU/Linux 12 (bookworm)" ]]; then
-  brew_path=/home/linuxbrew/.linuxbrew/bin
+if command -v lsb_release && [[ $(lsb_release -sd) == "Debian GNU/Linux 12 (bookworm)" ]]; then
   sudo DEBIAN_FRONTEND=noninteractive apt-get update --yes
   sudo DEBIAN_FRONTEND=noninteractive apt-get install --yes fish
 fi
 
-if ! command -v brew &> /dev/null; then
-  echo "Installing homebrew"
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  echo "eval \"\$($brew_path/brew shellenv)\"" >> $HOME/.bashrc
+if [[ $(uname) == "Darwin" ]]; then
+  brew_path=/opt/homebrew/bin
+else
+  brew_path=/home/linuxbrew/.linuxbrew/bin
 fi
 
 PATH=$brew_path:$PATH
+if command -v brew &> /dev/null; then
 brew bundle --file ./config/homebrew/Brewfile
+else
+  echo "homebrew is not installed; skipping 'brew bundle'"
+fi
 
-if [[ $(basename "$SHELL") != "fish" ]]; then
+if command -v fish &> /dev/null && [[ $(basename "$SHELL") != "fish" ]]; then
   fish_path=$(type -P fish)
   if ! grep -q -F "${fish_path}" /etc/shells; then
     echo "Adding ${fish_path} to /etc/shells"
