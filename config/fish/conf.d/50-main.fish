c98c310d8ac80cd900ffdde5279d2e57d789719f
Forrest Jacobs <forrestjacobs@gmail.com>
2025-08-06 22:21:13 -0400

    Move shared config into bash script

--- config/fish/conf.d/50-main.fish
+++ config/fish/conf.d/50-main.fish
@@ -3,57 +3,17 @@ if type -q term; and test (ps -p (string trim (ps -p $fish_pid -o ppid=)) -o com
   exec term
 end
 
-# homebrew
-if test -x /opt/homebrew/bin/brew
-  /opt/homebrew/bin/brew shellenv | source
-else if test -x /home/linuxbrew/.linuxbrew/bin/brew
-  /home/linuxbrew/.linuxbrew/bin/brew shellenv | source
-end
-
-# aliases/abbrs/functions
+# clear distro config
 functions -e ll
-
-abbr se 'sudo -e'
-
-if type -q eza
-  alias ls 'eza'
-  alias ll 'eza -aagl'
-  alias lll 'eza -glT --level=2'
-else
-  alias ll 'ls -al'
-end
-
-if type -q systemctl
-  abbr S 'sudo systemctl'
-end
-
-# XDG
-set -xg XDG_CONFIG_HOME "$HOME"/.config
 
 # Add bin
 fish_add_path "$HOME"/.local/bin
 
-# EDITOR
-set -xg EDITOR (get_editor)
-
-# bat
-set -xg BAT_STYLE numbers,changes
-set -xg BAT_THEME ansi
-
-# eza
-set -xg EZA_ICONS_AUTO
-set -xg TIME_STYLE iso
+# shared config
+init_shell fish | source
 
 # fish
 set -xg fish_greeting
-
-# less
-set -xg PAGER less
-set -xg LESS -iRFXMx4
-set -xg LESSHISTFILE ~/.cache/less-hist
-
-# man
-set -xg MANOPT --no-justification
 
 # hydro
 set -xg hydro_symbol_prompt ""
@@ -61,8 +21,3 @@ set -xg hydro_color_pwd      green
 set -xg hydro_color_git      cyan
 set -xg hydro_color_duration normal
 set -xg hydro_color_error    red
-
-# zoxide -- should be toward the end
-if type -q zoxide
-  zoxide init fish --cmd cd | source
-end
