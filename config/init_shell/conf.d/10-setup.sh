78af3f1c9165e73a55201be4b39271f3cbc67e54
Forrest Jacobs <forrestjacobs@gmail.com>
2025-08-21 18:21:37 -0400

    Don't reinitialize brew

--- config/init_shell/conf.d/10-setup.sh
+++ config/init_shell/conf.d/10-setup.sh
@@ -34,10 +34,12 @@ p_export XDG_CONFIG_HOME "$HOME"/.config
 add_path "$HOME"/.local/bin
 
 # Homebrew
-for brew_path in /opt/homebrew/bin/brew /home/linuxbrew/.linuxbrew/bin/brew; do
-  if has "$brew_path"; then
-    $brew_path shellenv "${shell:?}"
-    eval "$($brew_path shellenv bash)"
-    break
-  fi
-done
+if ! has brew; then
+  for brew_path in /opt/homebrew/bin/brew /home/linuxbrew/.linuxbrew/bin/brew; do
+    if has "$brew_path"; then
+      $brew_path shellenv "${shell:?}"
+      eval "$($brew_path shellenv bash)"
+      break
+    fi
+  done
+fi
