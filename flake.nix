f4994f62698d6e9be4131b8aee2694b2f6cf4578
Forrest Jacobs <forrestjacobs@gmail.com>
2025-08-30 14:30:21 -0400

    Attempt to fix issue sourcing init_shell in NixOS

--- flake.nix
+++ flake.nix
@@ -1,17 +1,23 @@
 {
   outputs = { ... }: {
-    homeModules.default = ({ lib, pkgs, config, ... }: {
+    homeModules.default = ({ lib, pkgs, config, ... }:
+    
+    let
+      dotfilesBin = (pkgs.stdenv.mkDerivation {
+        name = "dotfiles-bin";
+        src = ./bin;
+        installPhase = ''
+          mkdir -p $out/bin
+          cp * $out/bin
+        '';
+      });
+
+    in
+    {
       home.stateVersion = "23.11";
 
       home.packages = [
-        (pkgs.stdenv.mkDerivation {
-          name = "dotfiles-bin";
-          src = ./bin;
-          installPhase = ''
-            mkdir -p $out/bin
-            cp * $out/bin
-          '';
-        })
+        dotfilesBin
         pkgs.bat
         pkgs.eza
         pkgs.fish
@@ -20,6 +26,8 @@
         pkgs.patchutils
         pkgs.zoxide
       ];
+
+      home.file.".local/bin/init_shell".source = "${dotfilesBin}/bin/init_shell";
 
       home.file."${config.xdg.configHome}" = {
         source = ./config;
