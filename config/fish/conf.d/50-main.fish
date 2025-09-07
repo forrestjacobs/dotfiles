9d0d884e608805d7de6680fb293900f1f54f20a2
Forrest Jacobs <forrestjacobs@gmail.com>
2025-09-07 15:23:05 -0400

    Make sure tmux is installed before auto-term

--- config/fish/conf.d/50-main.fish
+++ config/fish/conf.d/50-main.fish
@@ -2,6 +2,6 @@
 ~/.local/bin/dot_shell fish | source
 
 # `exec term` if mosh-server is the parent
-if type -q term; and test (ps -p (string trim (ps -p "$fish_pid" -o ppid=)) -o comm=) = 'mosh-server'
+if type -q term; and type -q tmux; and test (ps -p (string trim (ps -p "$fish_pid" -o ppid=)) -o comm=) = 'mosh-server'
   exec term
 end
