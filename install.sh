1ef048ac5ad2a38f688bd9f71c5e2a3c5f181116
Forrest Jacobs <forrestjacobs@gmail.com>
2025-09-12 21:17:49 -0400

    Allow parent dotfiles in ~/dotfiles

--- install.sh
+++ install.sh
@@ -3,16 +3,54 @@ set -e
 
 pushd "$(dirname "${BASH_SOURCE:0}")" > /dev/null
 
-symlink="${HOME}/.local/dotfiles"
-target="$(pwd)"
-if [ ! -L "$symlink" ]; then
-  mkdir -p "$(dirname "$symlink")"
-  ln -s "$target" "$symlink"
-elif [ "$(readlink -f "$symlink")" != "$target" ]; then
-  echo "$symlink already exists and is pointing to something else; exiting"
-  exit 1
+echo 'Linking config files'
+./bin/dot_stow config "${HOME}/.config"
+./bin/dot_stow bin "${HOME}/.local/bin"
+
+echo
+echo 'Initializing shell'
+eval "$(shell=bash ./config/dotfiles/shell/10-setup.sh)"
+
+echo
+echo "Calling 'brew bundle'"
+if ! brew bundle --file ./config/homebrew/Brewfile; then
+  echo "homebrew failed; skipping"
 fi
 
-./bin/dot_install
+echo
+echo 'Configuring shells'
+add_line() {
+  if ! grep -qxF "$2" "${HOME}/$1"; then
+    echo "Updating $1"
+    echo "$2" >> "${HOME}/$1"
+  fi
+}
+# shellcheck disable=SC2016
+add_line .bashrc 'eval "$(~/.local/bin/dot_shell bash)"'
+# shellcheck disable=SC2016
+add_line .zshrc 'eval "$(~/.local/bin/dot_shell zsh)"'
+
+echo
+echo 'Configuring git'
+git_config() {
+  if [[ "$(git config --global "$1")" != "$2" ]]; then
+    git config --global "$1" "$2"
+  fi
+}
+if [ ! -f "${HOME}/.gitconfig" ] && [ ! -f "${HOME}/.config/git/config" ]; then
+  mkdir -p "${HOME}/.config/git"
+  touch "${HOME}/.config/git/config"
+  git_config user.name "Forrest Jacobs"
+  git_config user.email forrestjacobs@gmail.com
+fi
+git_config init.defaultBranch main
+git_config pull.ff only
+
+echo
+echo 'Done!'
+
+echo
+echo "Calling 'dot_doctor'"
+dot_doctor
 
 popd > /dev/null
