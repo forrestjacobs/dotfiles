f93fe787ca264109cd936b37e53379635bd94515
Forrest Jacobs <forrestjacobs@gmail.com>
2026-04-03 23:58:22 -0400

    Consolidate link step

--- install.sh
+++ install.sh
@@ -4,19 +4,11 @@ set -e
 pushd "$(dirname "${BASH_SOURCE:0}")" > /dev/null
 
 echo 'Linking config files'
-./home/.local/bin/dot_clean_links "${HOME}/.config"
-./home/.local/bin/dot_clean_links "${HOME}/.local/bin"
-./home/.local/bin/dot_clean_links "${HOME}/.local/share"
-./home/.local/bin/dot_stow home "${HOME}"
+./home/.local/bin/dot_relink
 
-echo
 echo 'Initializing shell'
 eval "$(shell=bash ./home/.config/dotfiles/shell/10-setup.sh)"
 
-echo
-echo 'Done!'
-
-echo
 echo "Calling 'dot_doctor'"
 dot_doctor
 
