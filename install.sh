163c3a930f1704f686e208de8e4d44741a91cc7b
Forrest Jacobs <forrestjacobs@gmail.com>
2026-04-04 00:13:47 -0400

    Avoid pushd

--- install.sh
+++ install.sh
@@ -1,15 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
-pushd "$(dirname "${BASH_SOURCE:0}")" > /dev/null
-
 echo 'Linking config files'
-./home/.local/bin/dot_relink
+"$(dirname "${BASH_SOURCE:0}")/home/.local/bin/dot_relink"
 
 echo 'Initializing shell'
-eval "$(shell=bash ./home/.config/dotfiles/shell/10-setup.sh)"
+eval "$(shell=bash "${HOME}/.config/dotfiles/shell/10-setup.sh")"
 
 echo "Calling 'dot_doctor'"
 dot_doctor
-
-popd > /dev/null
